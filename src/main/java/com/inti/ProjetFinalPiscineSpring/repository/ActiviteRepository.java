e91e8efa146a9ed0211345a128564699e3434182
CDegremont <103254745+CDegremont@users.noreply.github.com>
2022-06-09 10:56:47 +0200

    1commit -
    Ajout des models : Activite, Materiel, Planning
    Ajout des repository : Activite, Materiel, Planning,
    Ajout des controllers : Activite, Materiel, Planning,

---
+++ src/main/java/com/inti/ProjetFinalPiscineSpring/repository/ActiviteRepository.java
@@ -0,0 +1,10 @@
+package com.inti.ProjetFinalPiscineSpring.repository;
+
+import com.inti.ProjetFinalPiscineSpring.model.Activite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActiviteRepository extends JpaRepository<Activite, Integer>
+{
+
+}
